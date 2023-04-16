c665890e8ef041b8872eb9b309bfa1f52fb10dca
Software Engineer <im.ziad.sakr@gmail.com>
2023-04-16 01:13:20 -0700

    fix internal.rs

--- contracts/market-simple/src/internal.rs
+++ contracts/market-simple/src/internal.rs
@@ -4,7 +4,7 @@ use near_sdk::{CryptoHash};
 pub(crate) fn assert_one_yocto() {
     assert_eq!(
         env::attached_deposit(),
-        1,
+        2,
         "Requires attached deposit of exactly 1 yoctoNEAR"
     )
 }
